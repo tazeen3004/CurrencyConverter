8fa4a40ca56732b84abb328cd069dc3bd2eb7fae
tazeen khan <tazeenfirojkhan@gmail.com>
2017-02-16 23:49:53 +0530

    extracts currency rate from url

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
+            let url = URL(string: "http://api.fixer.io/latest")
+            
+            let task = URLSession.shared.dataTask(with:url!) { (data, response, error) in
+                
+                if (error != nil)
+                {
+                    print("ERROR")
+                }
+                else
+                {
+                    if let content = data
+                    {
+                        do
+                        {
+                            
+                            let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                            print (myJson)
+                        }
+                            
+                        catch
+                        {
+                            
+                        }
+                    }
+                    
+                }
+            }
+            task.resume()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
